23383572031574c494d7297b18d469599524e530
Devon Reed <devonreed@squareup.com>
2020-01-28 10:23:11 -0500

    Fork of Instagram Provider

---
+++ InstagramExtendSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialiteProviders\InstagramBasicDisplay;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class InstagramExtendSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param \SocialiteProviders\Manager\SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'instagram', __NAMESPACE__.'\Provider'
+        );
+    }
+}
